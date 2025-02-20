0328a68667a77c76bbcf3d0d6fa2b8812d8c5454
Balramrawat <110815042+Balramrawat@users.noreply.github.com>
2025-02-20 20:16:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,26 +2,34 @@
 function Car(make, model) {
 	this.make=make;
 	this.model=model;
-
-	car.prototype=function(){
-		return this.make+ " " + this.model;
-	}
+}
+	car.prototype.getMakeModel=function(){
+		return `${this.make} ${this.model}`;
 }
 
 function SportsCar(make, model, topSpeed) {
 
 	car.call(this, make, model);
 	this.topSpeed=topSpeed;
-
+}
 	SportsCar.prototype=object.create(car.prototype)
-	SportsCar.prototype.constructor=SportsCar
+	SportsCar.prototype.constructor=SportsCar;
+
 
 	SportsCar.prototype.getTopSpeed=function(){
 		return this.topSpeed;
-	}
+	};
 
-}
 
+
+// function showCarDetails() {
+//     const car1 = new SportsCar("Ferrari", "Testarossa", 200);
+//     const output = document.getElementById("output");
+
+//     output.innerHTML = `
+//         ${car1.getMakeModel()} <br>
+//         ${car1.getTopSpeed()}`;
+// }
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
